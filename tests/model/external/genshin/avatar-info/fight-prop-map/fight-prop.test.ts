893bf295677ab943b5035255cdc06ece4c5ed7b6
Carlos Felgueiras <catofel2000@gmail.com>
2024-01-10 02:41:26 +0000

    fmt

--- tests/model/external/genshin/avatar-info/fight-prop-map/fight-prop.test.ts
+++ tests/model/external/genshin/avatar-info/fight-prop-map/fight-prop.test.ts
@@ -1,17 +1,17 @@
 import { fightPropSchema } from "../../../../../../src/model/external/genshin/avatar-info/fight-prop-map/fight-prop";
 
-test('standard behaviour parsing', () => {
-    const data = 101;
+test("standard behaviour parsing", () => {
+  const data = 101;
 
-    const result = fightPropSchema.parse(data);
+  const result = fightPropSchema.parse(data);
 
-    expect(result).toEqual(data);
+  expect(result).toEqual(data);
 });
 
-test('missing value', () => {
-    const data = undefined;
+test("missing value", () => {
+  const data = undefined;
 
-    const result = fightPropSchema.parse(data);
+  const result = fightPropSchema.parse(data);
 
-    expect(result).toEqual(0);
+  expect(result).toEqual(0);
 });
