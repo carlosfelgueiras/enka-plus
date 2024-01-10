0b32629b27b766a852f571bab43481731b57ab72
Carlos Felgueiras <catofel2000@gmail.com>
2024-01-10 01:43:33 +0000

    fmt

--- tests/model/external/genshin/avatar-info/fetter-info.test.ts
+++ tests/model/external/genshin/avatar-info/fetter-info.test.ts
@@ -1,40 +1,38 @@
 import { fetterInfoSchema } from "../../../../../src/model/external/genshin/avatar-info/fetter-info";
 
-test('standard behaviour parsing', () => {
-    const input = {
-        expLevel: 10,
-    }
+test("standard behaviour parsing", () => {
+  const input = {
+    expLevel: 10,
+  };
 
-    const parsed = fetterInfoSchema.parse(input);
+  const parsed = fetterInfoSchema.parse(input);
 
-    expect(parsed).toEqual(input);
+  expect(parsed).toEqual(input);
 });
 
-test('out of range value for level', () => {
-    const inputOver = {
-        expLevel: 11,
-    }
+test("out of range value for level", () => {
+  const inputOver = {
+    expLevel: 11,
+  };
 
-    expect(() => fetterInfoSchema.parse(inputOver)).toThrow();
+  expect(() => fetterInfoSchema.parse(inputOver)).toThrow();
 
-    const inputUnder = {
-        expLevel: -1,
-    }
+  const inputUnder = {
+    expLevel: -1,
+  };
 
-    expect(() => fetterInfoSchema.parse(inputUnder)).toThrow();
+  expect(() => fetterInfoSchema.parse(inputUnder)).toThrow();
 });
 
-test('lack/extra properties', () => {
-    const inputLack = {
-    }
+test("lack/extra properties", () => {
+  const inputLack = {};
 
-    expect(() => fetterInfoSchema.parse(inputLack)).toThrow();
+  expect(() => fetterInfoSchema.parse(inputLack)).toThrow();
 
-    const inputExtra = {
-        expLevel: 10,
-        extra: 'extra',
-    }
+  const inputExtra = {
+    expLevel: 10,
+    extra: "extra",
+  };
 
-    
-    expect(() => fetterInfoSchema.parse(inputExtra)).not.toThrow();
+  expect(() => fetterInfoSchema.parse(inputExtra)).not.toThrow();
 });
