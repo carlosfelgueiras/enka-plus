aff932defd6d90b0dfe31b61049805e08a0c7797
Carlos Felgueiras <catofel2000@gmail.com>
2024-01-10 01:55:06 +0000

    test: added unit tests for prop and prop-map parsing

---
+++ tests/model/external/genshin/avatar-info/prop.test.ts
@@ -0,0 +1,39 @@
+import { propSchema } from "../../../../../src/model/external/genshin/avatar-info/prop";
+
+test("standard behaviour parsing", () => {
+  const data = {
+    type: 1,
+    val: "2",
+  };
+
+  const result = propSchema.parse(data);
+
+  expect(result).toEqual(data);
+});
+
+test("parsing without value", () => {
+  const data = {
+    type: 1,
+  };
+
+  const expected = {
+    type: 1,
+    val: "0",
+  };
+
+  const result = propSchema.parse(data);
+
+  expect(result).toEqual(expected);
+});
+
+test("parsing without type", () => {
+  const dataNoType = {
+    val: "2",
+  };
+
+  expect(() => propSchema.parse(dataNoType)).toThrow();
+
+  const dataEmpty = {};
+
+  expect(() => propSchema.parse(dataEmpty)).toThrow();
+});
