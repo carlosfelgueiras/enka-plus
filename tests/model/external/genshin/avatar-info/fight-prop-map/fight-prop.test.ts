84b3e12f4ec05050d59b8e0b701eaeaaa317442e
Carlos Felgueiras <catofel2000@gmail.com>
2024-01-12 03:18:40 +0000

    feature: migration from jest to vitest done, changed all tests to import vitest functions (contrary to jest where the functions where on the global scope)

--- tests/model/external/genshin/avatar-info/fight-prop-map/fight-prop.test.ts
+++ tests/model/external/genshin/avatar-info/fight-prop-map/fight-prop.test.ts
@@ -1,3 +1,4 @@
+import { test, expect } from "vitest";
 import { fightPropSchema } from "../../../../../../src/model/external/genshin/avatar-info/fight-prop-map/fight-prop";
 
 test("standard behaviour parsing", () => {
